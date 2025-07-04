45df8268f8a566742433689e9bd6bb674167f61e
gurinKANIMAYO <risshi358@gmail.com>
2025-07-04 21:58:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // すべてのリクエストをkanimayo.f5.siにプロキシ
 app.use('/', createProxyMiddleware({
-  target: 'https://kanimayo.f5.si',
+  target: 'https://discord.com/',
   changeOrigin: true,
   ws: true,
   secure: false // 証明書が自己署名などの場合に必要
