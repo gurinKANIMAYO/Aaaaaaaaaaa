c2a68ce9453a68cb85d258c9c8f0341796cc5ee3
gurinKANIMAYO <risshi358@gmail.com>
2025-07-15 22:27:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // すべてのリクエストをkanimayo.f5.siにプロキシ
 app.use('/', createProxyMiddleware({
-  target: 'https://suspended-domain.net/',
+  target: 'https://www.werewolfgame.jp/',
   changeOrigin: true,
   ws: true,
   secure: false // 証明書が自己署名などの場合に必要
